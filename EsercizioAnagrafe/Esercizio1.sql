9cb86a9a119189781c2d79380ef3a0399f3a837f
Pelux94 <peluso94@hotmail.it>
2022-05-02 15:37:56 +0200

    seconda iterazione

--- EsercizioAnagrafe/Esercizio1.sql
+++ EsercizioAnagrafe/Esercizio1.sql
@@ -1,9 +1,3 @@
-select distinct Persona.Nome, Città.Regione
-from Persona, Genia, Città
-where (Persona.Nome = Genia.Figlio and Persona.CittàNascita = Città.Nome) and Persona.Età > 18
-
-/* se provo a mettere Persona.Nome = Genia.Figlio and Persona.Nome = Genia.Genitore non funziona */
-
-select distinct Persona.Nome, Città.Regione
-from Persona, Genia, Città
-where (Persona.Nome = Genia.Genitore and Persona.CittàNascita = Città.Nome) and Persona.Età > 18
+select Persona.Nome, Città.Regione
+from Persona, Città
+where Persona.CittàNascita = Città.Nome and Persona.Età > 18
