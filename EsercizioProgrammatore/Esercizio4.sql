baa3c0646837712df51164daddde45c40879c537
Pelux94 <peluso94@hotmail.it>
2022-05-03 11:51:07 +0200

    seconda iterazione

--- EsercizioProgrammatore/Esercizio4.sql
+++ EsercizioProgrammatore/Esercizio4.sql
@@ -1,4 +1,4 @@
-select Programmatore.codice
+select distinct Programmatore.codice
 from Programmatore join Autore on Programmatore.codice = Autore.codice 
     join Programma on Programma.id = Autore.id
 where Programma.linguaggio = "Python"
