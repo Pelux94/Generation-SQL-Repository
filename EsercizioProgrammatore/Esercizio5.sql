a580f2bc61056b6e6c417edf7757bb2a39184c94
Pelux94 <peluso94@hotmail.it>
2022-05-03 12:49:50 +0200

    seconda iterazione

--- EsercizioProgrammatore/Esercizio5.sql
+++ EsercizioProgrammatore/Esercizio5.sql
@@ -1,4 +1,5 @@
 select Programmatore.codice, Programmatore.nome
 from Programmatore join Autore on Programmatore.codice = Autore.codice 
     join Programma on Programma.id = Autore.id
-where Programma.linguaggio = "Java"
+where programma.linguaggio = "Java"
+group by programma.linguaggio = "Java"
