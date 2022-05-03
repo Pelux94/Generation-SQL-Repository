f1d046b314cd9dafd7a60c428963cb2165623107
Pelux94 <peluso94@hotmail.it>
2022-05-03 11:10:15 +0200

    prima iterazione

---
+++ EsercizioProgrammatore/Esercizio1.sql
@@ -0,0 +1,3 @@
+select count(Programmatore.codice)
+from Programmatore join Autore on Programmatore.codice = Autore.codice join Programma on Programma.id = Autore.id
+where Programma.linguaggio = "Java" and Programma.anno > 2000
