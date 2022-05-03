a32a18706340d7edc5cd021f51ac9ca791903fe7
Pelux94 <peluso94@hotmail.it>
2022-05-03 11:31:11 +0200

    prima iterazione

---
+++ EsercizioProgrammatore/Esercizio4.sql
@@ -0,0 +1,4 @@
+select Programmatore.codice
+from Programmatore join Autore on Programmatore.codice = Autore.codice 
+    join Programma on Programma.id = Autore.id
+where Programma.linguaggio = "Python"
