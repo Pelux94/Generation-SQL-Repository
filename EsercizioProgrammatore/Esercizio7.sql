c94cc7191381d9ab3db9417e2bf6ccbb853ceddf
Pelux94 <peluso94@hotmail.it>
2022-05-04 10:04:40 +0200

    terza iterazione

--- EsercizioProgrammatore/Esercizio7.sql
+++ EsercizioProgrammatore/Esercizio7.sql
@@ -1,4 +1,4 @@
-select distinct programma.linguaggio, count(Programmatore.codice) /* ha detto che non è una media */
+select distinct programma.linguaggio, count(*)/count(distinct programma.id) /* ha detto che non è una media */
 from Programmatore join Autore on Programmatore.codice = Autore.codice 
     join Programma on Programma.id = Autore.id
 group by programma.linguaggio
