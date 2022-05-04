fa513be0eec259eca9adcfad93dcad0a02dff6a0
Pelux94 <peluso94@hotmail.it>
2022-05-04 12:10:09 +0200

    seconda iterazione

--- EsercizioScalatori/Esercizio4.sql
+++ EsercizioScalatori/Esercizio4.sql
@@ -1,2 +1,5 @@
-select scalatore.cf, nazione.nome
-from Scalatore join Nazione on Scalatore.nazioneNascita = nazione.nome 
+select scalatore.cf, nazione.nome, count(*)
+from Scalatore join Scalata on Scalatore.cf = Scalata.scalatore 
+    join Nazione on Scalata.nazione = nazione.nome
+where scalatore.nazioneNascita = Scalata.nazione
+group by nazione.nome
