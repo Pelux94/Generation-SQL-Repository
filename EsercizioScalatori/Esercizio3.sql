00739c13a20a33b8675608f1e01518bf8efc197a
Pelux94 <peluso94@hotmail.it>
2022-05-04 11:41:08 +0200

    prima iterazione

---
+++ EsercizioScalatori/Esercizio3.sql
@@ -0,0 +1,4 @@
+select scalata.nazione, nazione.continente
+from Scalatore join Scalata on Scalatore.cf = Scalata.scalatore 
+    join Nazione on Scalata.nazione = nazione.nome
+where scalata.anno - scalatore.annoNascita > 18
